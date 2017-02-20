585f36361813a754a70a84dbb923d21f330e7afc
BruceZhong <zhdejiang@126.com>
2017-02-20 18:24:50 +0800

    数据呈报模块代码修改

--- basequery-web/src/main/java/com/eenet/basequery/datareport/DRStudentDataReportController.java
+++ basequery-web/src/main/java/com/eenet/basequery/datareport/DRStudentDataReportController.java
@@ -15,17 +15,17 @@ import com.eenet.basequery.page.Pagination;
 @RequestMapping("/datareport")
 public class DRStudentDataReportController {
 	
-//	@Autowired
-//	private DRStudentDataReportService drStudentDataReportService;
+	@Autowired
+	private DRStudentDataReportService dRStudentDataReportService;
 	
 	@RequestMapping(value="/query")
 	public String query(HttpSession session,QueryCondition queryCondition,Pagination pagination ,Model model){
 		queryCondition.setMaxQuantity(pagination.getPageSize());
 		queryCondition.setStartIndex(pagination.getIndexCurrent());
-//		SimpleResultSet<DRStudentDataReport> result = drStudentDataReportService.query(queryCondition);
-//		pagination.setTotal(result.getCount());
-//		model.addAttribute("resultMap", result);
-//		model.addAttribute("Pagination", pagination);
+		SimpleResultSet<DRStudentDataReport> result = dRStudentDataReportService.query(queryCondition);
+		pagination.setTotal(result.getCount());
+		model.addAttribute("resultMap", result);
+		model.addAttribute("Pagination", pagination);
 		
 		return "datareport/DRStudentList";
 	}
